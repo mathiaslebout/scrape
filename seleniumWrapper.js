1255b4643f439e536bee695c9b2d03243dce5720
Mathias Lebout <mathias.lebout@gmail.com>
2016-12-27 19:43:13 +0100

    runas ec2-user

--- seleniumWrapper.js
+++ seleniumWrapper.js
@@ -37,7 +37,7 @@ const seleniumStart = (callback) => {
                     baseURL: 'https://selenium-release.storage.googleapis.com/'
                 }
             },
-        javaArgs: ['-Xmx=512M'],
+        // javaArgs: ['-Xmx=512M'],
     }, 
         (err, child) => {
             if (err) {
